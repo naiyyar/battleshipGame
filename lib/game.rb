7c91f8e9b8c22c1807d32bc3ec4d4c5ecef9f4d7
Naiyyar <naiyyar.abbas512013@gmail.com>
2024-03-25 14:23:42 +0530

    hitting and displaying results

--- lib/game.rb
+++ lib/game.rb
@@ -1,3 +1,4 @@
+require 'byebug'
 require_relative './player.rb'
 
 # Need to take inputs 
@@ -10,26 +11,67 @@ require_relative './player.rb'
 
 class Game
   def initialize
-    @grid_size = get_grid_size
-    @total_ships = get_ship_size
+    @grid_size = 5 || get_grid_size
+    @total_ships = 5 || get_ship_size
+    @player1_hits = @player2_hits = 0
+    @player1 = Player.new(name: 'Player 1', grid_size: @grid_size, moves: '0,1:4,3:2,3:3,1:4,1')
+    @player2 = Player.new(name: 'Player 2', grid_size: @grid_size, moves: '0,1:0,0:1,1:2,3:4,3')
   end
 
   def start
-    # Initialize players grids
-    player1 = Player.new(name: 'Player 1', grid_size: @grid_size)
-    player2 = Player.new(name: 'Player 2', grid_size: @grid_size)
-
     # get p1 & p2 ships positions and place ships on the grid
-    p1_ships_positions = get_p1_ships_positions
-    p2_ships_positions = get_p2_ships_positions
+    place_ships
 
-    player1.postions_ships(p1_ships_positions)
-    player2.postions_ships(p2_ships_positions)
+    total_missiles = 5 || get_total_missiles
 
-    player1.display_grid
+    if total_missiles <= 0
+      puts "Number of missiles should be greater than 0"
+      return
+    end
+
+    p1_moves = @player1.get_moves
+    p2_moves = @player2.get_moves
+
+    puts "Player 1's turn:"
+    @player2.display_grid
+    p2_moves.each do |move|
+      has_hit = @player1.hit(*move)
+    end
+
+    puts "Player 2's turn:"
+    @player1.display_grid
+    p1_moves.each do |move|
+      has_hit = @player2.hit(*move)
+    end
+    
+    display_results
   end
 
   private
+
+  def display_results
+    puts 'Player1'
+    print @player1.display_grid
+    puts ''
+    puts 'Player1'
+    print @player2.display_grid
+    puts ''
+    puts "P1:#{@player1.hits_count}"
+    puts "P2:#{@player2.hits_count}"
+    
+    if @player1_hits > @player2_hits
+      puts 'Player 1 wins'
+    elsif @player2_hits > @player1_hits
+      puts 'Player 2 wins'
+    else
+      puts 'It is a draw'
+    end
+  end
+
+  def place_ships
+    @player1.position_ships(get_p1_ships_positions)
+    @player2.position_ships(get_p2_ships_positions)
+  end
 
   def get_grid_size
     puts 'Enter grid size (e.g., 4):'
@@ -43,12 +85,16 @@ class Game
 
   def get_p1_ships_positions
     puts 'Enter player1 ships positions (x,y pairs separated by colon, eg; 1,1:2,0:2,3:3,4,...):'
-    gets.chomp.split(':').map{|rc| rc.split(",").map(&:to_i)}
+    '1,1:2,0:2,3:3,4:4,3'.split(':').map{|rc| rc.split(",").map(&:to_i)}
   end
 
   def get_p2_ships_positions
     puts 'Enter player2 ships positions (x,y pairs separated by colon, eg; 1,1:2,0:2,3:3,4,...):'
-    gets.chomp.split(':').map{|rc| rc.split(",").map(&:to_i)}
+    '0,1:2,3:3,0:3,4:4,1'.split(':').map{|rc| rc.split(",").map(&:to_i)}
   end
 
+  def get_total_missiles
+    puts 'Enter missiles size (e.g., 4):'
+    gets.chomp.to_i
+  end
 end
--- lib/player.rb
+++ lib/player.rb
@@ -1,20 +1,42 @@
+# display grid
+# palce ship postions on grid
+# hit results
+# get moves
+
 class Player
-  attr_accessor :name
+  attr_accessor :name, :hits_count
   
-  def initialize name:, grid_size:
+  def initialize name:, grid_size:, moves:
     @name = name
     @grids = Array.new(grid_size){ Array.new(grid_size, '_') }
+    @hits_count = 0
+    @move = moves
   end
 
   def display_grid
     @grids.each{|grid| puts grid.join(' ')}
   end
 
-  def postions_ships positions
+  def position_ships positions
     positions.each do |position|
       # position => [1,2]
       row, col = position
       @grids[row][col] = 'B'
     end
+  end
+
+  def hit(row, col)
+    # byebug
+    if @grids[row][col] == 'B'
+      @grids[row][col] = 'X'
+      self.hits_count += 1
+    else
+      @grids[row][col] = 'O'
+    end
+  end
+
+  def get_moves
+    puts "Enter #{name} moves (x,y pairs separated by colon, eg; 1,1:2,0:2,3:3,4,..):"
+    @move.split(':').map{|rc| rc.split(",").map(&:to_i)}
   end
 end
